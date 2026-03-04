8f1de2ca3b02ea9dbf32fdd7f4f960e1afdbef87
Shariare Emon <Shahriareemon2580@gmail.com>
2026-03-04 15:16:58 +0600

    callback.js

---
+++ callback.js
@@ -0,0 +1,21 @@
+// document.getElementById('#btn').addEventListener('click', function(event) {
+
+// })
+//opore callback function
+
+function settelLife (name, isBcs) {
+
+    if(isBcs===true) {
+       console.log('valo') 
+    }
+    else{
+        console.log('tahole ar ki')
+    }
+
+    function biyeKorbo() {
+        console.log0
+    }
+}
+
+
+settelLife('Emon', false)
--- closure.js
+++ closure.js
@@ -9,17 +9,32 @@ const result = outerFunction();
 // result()
 // console.log('This is outside', output)
 
-function counter () {
+function counter (owner) {
     let count = 0;
     function increment() {
         count = count + 1;
-        console.log('value of count',count)
+        console.log('value of count',owner ,count)
     }
     return increment;
 }
 const count1 = counter()
 //  console.log(count1)
-count1()
-count1()
-count1()
-count1()
+// count1()
+// count1()
+// count1()
+// count1()
+
+const emonCounter = counter('Emon');
+emonCounter()
+emonCounter()
+emonCounter()
+emonCounter()
+emonCounter()
+emonCounter()
+emonCounter()
+
+const mamunCounter = counter('Mamun');
+mamunCounter()
+mamunCounter()
+mamunCounter()
+mamunCounter()
